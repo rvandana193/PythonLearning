9e9abc5ccbbd7aae0b96eae9a91eac0f0c017882
Vandana Rai <rvandana292@gmail.com>
2023-11-06 19:05:28 +0530

    Lists

--- day1.py
+++ day1.py
@@ -13,3 +13,16 @@ print('This is a string {}'.format('INSERTED'))
 print('The {} {} {}'.format('fox','quick','brown'))
 print('The {0} {0} {0}'.format('fox','quick','brown'))
 print('The {q} {b} {f}'.format(f='fox',q='quick',b='brown'))
+
+my_list=['one','two','three']
+my_lists=['four','five']
+print(my_list+my_lists)
+print(my_list[0])
+my_list.append('six')
+print(my_list)
+my_list.pop()
+print(my_list)
+new_list= ['a','x','c']
+new_list.sort()
+print(new_list)
+
