d3a8dd7597fcadd293d93eaf6361024f90a27c6c
Vandana Rai <rvandana292@gmail.com>
2023-11-06 18:40:51 +0530

    Print Formatting

--- day1.py
+++ day1.py
@@ -9,3 +9,7 @@ print(mystring[2:4])
 x="Hi This Is A String"
 print(x.split())
 
+print('This is a string {}'.format('INSERTED'))
+print('The {} {} {}'.format('fox','quick','brown'))
+print('The {0} {0} {0}'.format('fox','quick','brown'))
+print('The {q} {b} {f}'.format(f='fox',q='quick',b='brown'))
