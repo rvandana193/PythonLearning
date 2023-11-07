0d7a0f167220d60e4269811520a4853371f97ad6
Vandana Rai <rvandana292@gmail.com>
2023-11-07 14:32:02 +0530

    methods and functions

---
+++ day2.py
@@ -0,0 +1,60 @@
+name = 'Sam'
+if name == 'John':
+    print("Hi John!")
+elif name == 'Sam':
+    print("Hi Sam!")
+else:
+    print("What is your name?")
+
+
+
+mylist = [1,2,3,4,5,6]
+for num in mylist:
+    if num%2 == 0:
+        print(num)
+    else:
+        print(f'Odd Number : {num}')
+
+
+tup = (1,2,3)
+for item in tup:
+    print(item)
+
+
+mylist = [(1,2),(3,4),(5,6)]
+for item in mylist:
+    print(item)
+for a,b in mylist:
+    print(a)
+    print(b)
+
+
+d={'k1':1,'k2':2,'k3':3}
+for item in d.items():
+    print(item)
+for key,value in d.items():
+    print(value)
+
+
+
+x = [1,2.3]
+for item in x:
+    pass
+print("End of my script")
+
+
+mystring = 'Sammy'
+for letter in mystring:
+    if letter == 'a':
+        continue
+    print(letter)
+
+
+x=0
+while x<5:
+    if x==2:
+        break
+    print(x)
+    x+=1
+    
+
