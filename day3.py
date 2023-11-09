06f74d09f623f9ea01f012624a00e71b1c5e1606
Vandana Rai <rvandana292@gmail.com>
2023-11-09 11:31:04 +0530

    Attributes

---
+++ day3.py
@@ -0,0 +1,9 @@
+class Dog():
+    def __init__(self,breed,name,spots):
+        self.breed=breed
+        self.name=name
+        self.spots=spots
+my_dog= Dog(breed='Lab',name='Shane',spots='No')
+print(my_dog.breed)
+print(my_dog.name)
+print(my_dog.spots)
