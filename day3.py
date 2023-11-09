7860c92988000424a20ae045cd70a498d515e2c7
Vandana Rai <rvandana292@gmail.com>
2023-11-09 11:38:57 +0530

    Method

--- day3.py
+++ day3.py
@@ -7,3 +7,15 @@ my_dog= Dog(breed='Lab',name='Shane',spots='No')
 print(my_dog.breed)
 print(my_dog.name)
 print(my_dog.spots)
+
+
+class Circle():
+    pi = 3.14 #class-object attribute
+    def __init__(self,radius=1):
+        self.radius=radius
+        self.area=radius*radius*Circle.pi
+    def get_circumference(self):
+        return self.radius*Circle.pi*2
+my_circle=Circle(30)
+print(my_circle.radius)
+print(my_circle.get_circumference())
